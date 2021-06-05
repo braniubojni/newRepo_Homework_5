61c90628144ec6c97dbe294b9c3ae710a197f915
Erik Harutyunyan <braniubojni@mail.ru>
2021-06-05 10:03:02 +0400

    Update homework.js

--- homework.js
+++ homework.js
@@ -1,21 +1,25 @@
 // 1) Deep copy function
 {
-		const deepCopy = obj => {
-	    let newObj = {};
-	    for(let key in obj){
-	        if(Array.isArray(obj[key])){
-	            newObj[key] = obj[key];
-	        } else if(typeof obj[key] !== 'object'){
-	            newObj[key] = obj[key];
-	        } else {
-	            newObj[key] = deepCopy(obj[key]);
-	        }
-	    }
-	    return newObj;
+	const deepCopy = obj => {
+		let newObj = {};
+		for(let key in obj){
+			let arr = [];
+			if(Array.isArray(obj[key])){
+				obj[key].forEach(item => arr.push(item))
+				newObj[key] = arr;
+			} else if(typeof obj[key] !== 'object'){
+				newObj[key] = obj[key];
+			} else {
+				newObj[key] = deepCopy(obj[key]);
+			}
+		}
+		return newObj;
 	};
-	// var a = { a: '1111', b: { a: 2}, c:[1,2,3] };
-	// var b = deepCopy(a);
-	// a.b.a = 123
-	// console.log(a);
-	// console.log(b);
+	var a = { a: '1111', b: { a: 2}, c:[1,2,3] };
+	var b = deepCopy(a);
+	a.b.a = 123
+
+	console.log(b.c.push(5));
+	console.log(a);
+	console.log(b);
 }
