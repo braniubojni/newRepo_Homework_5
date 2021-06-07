7af53256833e6a06864f2c26e157ab1f02b26db9
Erik Harutyunyan <braniubojni@mail.ru>
2021-06-07 12:24:37 +0400

    Update homework.js

--- homework.js
+++ homework.js
@@ -1,4 +1,4 @@
-// 1) Deep copy function
+// N1 Deep copy function
 {
 	const deepCopy = obj => {
 		let newObj = {};
@@ -22,4 +22,34 @@
 	console.log(b.c.push(5));
 	console.log(a);
 	console.log(b);
+}
+// N3
+{
+	const mapRange2 = (arr,x,y) => {
+		arr = arr.slice(arr.indexOf(x),arr.lastIndexOf(y)+1)
+		let mapObj = new Map(),
+			obj = {},
+			dublicates = [],
+			temp2 = [],
+			numArr = [];
+
+		for(let key of arr){
+			if(obj.hasOwnProperty(obj[key])){
+				dublicates.push(key)
+			}
+			obj[key] = key
+			mapObj.set(key, `${key}`)
+		}
+		numArr = Object.keys(obj).map(item => +item)
+		temp2 = Object.keys(obj).map(item => +item)
+
+		for(let i in temp2){
+			if(dublicates.includes(temp2[i])){
+				numArr.splice(numArr.indexOf(temp2[i]), 0, temp2[i])
+			}
+		}
+
+		return [arr, numArr]
+	}
+	console.log(mapRange2([-5, 3, 2, 5,3, -9, 6, 3, 11, 6], 2, 6))
 }
